acc163eedab2a6cd0babb3957b920d0d5993de5f
Yoshihiro.Matsuzaki <yoshihiro.matsuzaki@mnlg-az.com>
2024-05-21 13:44:36 +0900

    更新

---
+++ CompositeSignal/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CompositeSignal {
+	class Program {
+
+		static void Main(string[] args) {
+			if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0]) || !File.Exists(args[0])) {
+				return;
+			}
+			var path = args[0];
+			var img = new Bitmap(path);
+			var width = img.Width;
+			var height = img.Height;
+			var data = img.LockBits(
+				new Rectangle(0, 0, img.Width, img.Height),
+				ImageLockMode.ReadOnly,
+				PixelFormat.Format24bppRgb
+			);
+			var stride = data.Stride;
+			var arrData = new byte[data.Stride * data.Height];
+			Marshal.Copy(data.Scan0, arrData, 0, arrData.Length);
+			img.UnlockBits(data);
+			img.Dispose();
+
+			if (true) {
+				var outPath = Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path);
+				var sw = new FileStream(outPath, FileMode.Create);
+				var yiq = new YIQ();
+				var ptr = 0;
+				for (int y = 0; y < height; y++) {
+					int x = 0, p = ptr;
+					while (x < width) {
+						double output = 0;
+						if (yiq.Encode(ref output, arrData, p, x == 0)) {
+							x++;
+							p += 3;
+						}
+						sw.WriteByte((byte)(output * 127 + 128));
+					}
+					ptr += stride;
+				}
+				sw.Close();
+				sw.Dispose();
+			}
+
+			if (true) {
+				var outPath = Path.GetDirectoryName(path) + "\\" + Path.GetFileNameWithoutExtension(path) + ".csv";
+				var sw = new StreamWriter(outPath);
+				var yiq = new YIQ();
+				var ptr = 0;
+				var s1 = 0.0;
+				var s2 = 0.0;
+				sw.WriteLine("Input,S0,Y,C");
+				for (int y = 0; y < height && y < 2; y++) {
+					int x = 0, p = ptr;
+					while (x < width) {
+						double output = 0;
+						if (yiq.Encode(ref output, arrData, p, x == 0)) {
+							x++;
+							p += 3;
+						}
+						var d = (byte)(output * 127 + 128);
+						var s0 = (d - 128) / 128.0;
+						var lum = s0 + s2;
+						var chroma = (s0 - s2) * 3 / 2;
+						if (lum < 0) {
+							lum *= -1;
+						}
+						sw.WriteLine("{0},{1},{2},{3}",
+							d,
+							s0.ToString("0.00"),
+							lum.ToString("0.00"),
+							chroma.ToString("0.00")
+						);
+						s2 = s1;
+						s1 = s0;
+					}
+					ptr += stride;
+				}
+				sw.Close();
+				sw.Dispose();
+			}
+		}
+
+	}
+}
---
+++ CompositeSignal/YIQ.cs
@@ -0,0 +1,119 @@
+﻿namespace CompositeSignal {
+	class YIQ {
+		const int SyncTriggerBegin = 8;
+		const int SyncTriggerMid = 15;
+		const int SyncTriggerEnd = 22;
+		const int SyncChromaBegin = 30;
+		const int SyncChromaEnd = 50;
+		const int SyncSamples = 60;
+
+		static readonly int[] Y = { 1, -1 };
+		static readonly int[] Re = { 1, 0,-1, 0 };
+		static readonly int[] Im = { 0, 1, 0, -1 };
+
+		int mCount = 0;
+		int mEnableSync = 0;
+		int mIdxY = 0;
+		int mIdxC = 0;
+		int mOddEven = 0;
+
+		double mT = 0;
+		double mY = 0;
+		double mCi = 0;
+		double mCq = 0;
+		double mChromaSyncAmp = 0;
+
+		double mS1, mS2;
+
+		public bool Encode(ref double output, byte[] pix, int ptr, bool sync) {
+			if (0 == mEnableSync && sync) {
+				mEnableSync = 1;
+				mCount = 0;
+			}
+
+			int enableSignal;
+			int enableSync;
+			var trigger = 0.0;
+			if (0 == mEnableSync) {
+				/* 状態：信号出力 */
+				enableSignal = 1;
+				enableSync = 0;
+			} else if (mCount < SyncTriggerBegin) {
+				/* 状態：同期開始 */
+				enableSignal = 0;
+				enableSync = 0;
+			} else if (mCount < SyncTriggerEnd) {
+				/* 状態：トリガー出力 */
+				enableSignal = 0;
+				enableSync = 0;
+				mIdxY = mOddEven;
+				mIdxC = mOddEven * 2;
+				trigger = mCount < SyncTriggerMid ? -0.9 : 0.9;
+			} else {
+				/* 状態：クロマ同期出力 */
+				enableSignal = 0;
+				if (SyncChromaBegin <= mCount && mCount < SyncChromaEnd) {
+					enableSync = 1;
+				} else {
+					enableSync = 0;
+				}
+			}
+
+			/* RGB -> YIQ */
+			var r = pix[ptr];
+			var g = pix[ptr + 1];
+			var b = pix[ptr + 2];
+			var y = (0.299 * r + 0.587 * g + 0.114 * b) * 0.5 / 255;
+			var ci = (0.596 * r - 0.274 * g - 0.322 * b) * 0.333 / 255;
+			var cq = (0.211 * r - 0.522 * g + 0.311 * b) * 0.333 / 255;
+			/* トリガー */
+			mT += (trigger - mT) * 0.5;
+			/* 輝度振幅 */
+			mY += (enableSignal * y - mY) * 0.5;
+			/* クロマ信号振幅 */
+			mCi += (enableSignal * ci - mCi) * 0.333;
+			mCq += (enableSignal * cq - mCq) * 0.333;
+			/* クロマ同期振幅 */
+			mChromaSyncAmp += (enableSync * 0.2 - mChromaSyncAmp) * 0.333;
+			/* 出力 */
+			output = mT + Y[mIdxY] * mY + Re[mIdxC] * mCi + Im[mIdxC] * (mCq + mChromaSyncAmp);
+			/* 輝度発振器 */
+			mIdxY++;
+			mIdxY -= mIdxY / 2 * 2;
+			/* クロマ発振器 */
+			mIdxC++;
+			mIdxC -= mIdxC / 4 * 4;
+
+			mCount++;
+			if (0 < mEnableSync && SyncSamples < mCount) {
+				mEnableSync = 0;
+				mOddEven = (mOddEven + 1) % 2;
+			}
+			return 0 == mEnableSync;
+		}
+
+		public bool Decode(byte input, ref byte r, ref byte g, ref byte b) {
+			// Y = |s0 + s2|
+			// C = (s0 - s2)*2
+			var s0 = (input - 128) / 128.0;
+			var y = s0 + mS2;
+			var c = (s0 - mS2) * 2;
+			mS2 = mS1;
+			mS1 = s0;
+
+			var cre = c * Re[mIdxC];
+			var cim = c * Im[mIdxC];
+			mIdxC++;
+			mIdxC -= mIdxC / 4 * 4;
+
+			if (y < 0) {
+				y *= -1;
+			}
+
+			//R = y + 0.956 * ci + 0.623 * cq;
+			//G = y - 0.272 * ci - 0.648 * cq;
+			//B = y - 1.105 * ci + 0.705 * cq;
+			return true;
+		}
+	}
+}
